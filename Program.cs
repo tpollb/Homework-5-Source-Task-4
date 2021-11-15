dd8677e48ce8e0f9b5c7b56b817f104432571711
Сергей Усков <q3mydoom@hotmail.com>
2021-11-15 11:40:35 +0500

    123

--- Program.cs
+++ Program.cs
@@ -9,9 +9,8 @@ namespace Homework_5_Source_Task_4
         /// метод, принимающий последовательность чисел и определяющий является последовательность арифметической или геометрической прогрессией
         /// </summary>
         /// <param name="Source">Исходная последовательность</param>
-        static void IsArOrGeoProg(string Source)
+        static int IsArOrGeoProg(int[] iArr)
         {
-            int[] iArr = Source.Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
 
             int flagAr = 0;
             int flagGeo = 0;
@@ -33,19 +32,20 @@ namespace Homework_5_Source_Task_4
                     flagGeo++;
                 }
             }
-            if (flagAr != 0)
-            {
-                Console.WriteLine("Не является арифметической прогрессией");
-            } else Console.WriteLine("Является арифметической прогрессией");
 
-            if (flagGeo != 0)
+            if (flagAr == 0)
             {
-                Console.WriteLine("Не является геометрической прогрессией");
-            }
-            else Console.WriteLine("Является геометрической прогрессией");
+                return 1;
+            } else if (flagGeo == 0)
+            {
+                return 2;
+            } else return 0;
         }
         static void Main(string[] args)
         {
+            bool flag = default;
+            string str;
+
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Тема 5. Домашнее задание.\n");
             // Задание 3
@@ -53,9 +53,33 @@ namespace Homework_5_Source_Task_4
             Console.WriteLine("Задание 4. Создать метод, принимающий последовательность чисел " +
                 "и определяющий является последовательность арифметической или геометрической прогрессией\n");
             Console.ResetColor();
-            Console.WriteLine("Введите последовательность чисел\n");
-            string str = Console.ReadLine();
-            IsArOrGeoProg(str);
+            Console.WriteLine("Введите последовательность чисел, разделённых пробелом\n");
+
+            str = Console.ReadLine();
+            var strWithoutSpaces = str.Replace(" ", "");
+            flag = int.TryParse(strWithoutSpaces, out int tmp);
+
+            while (flag == false)
+            {
+                Console.WriteLine("Введите последовательность чисел, разделённых пробелом\n");
+                str = Console.ReadLine();
+                flag = int.TryParse(strWithoutSpaces, out tmp);
+            }
+            
+            int[] iArr = str.Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+
+            int a = IsArOrGeoProg(iArr);
+            if (a == 1)
+            {
+                Console.WriteLine("Последовательность чисел является арифметической прогрессией\n");
+            } else if (a == 2)
+            {
+                Console.WriteLine("Последовательность чисел является геометрической прогрессией\n");
+            } else if (a == 0)
+            {
+                Console.WriteLine("Последовательность чисел не является ни арифметической ни геометрической прогрессией\n");
+            }
+
 
             Console.ReadKey();
         }
